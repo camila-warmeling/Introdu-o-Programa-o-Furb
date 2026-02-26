b6eefa727e79b9e0c19e1643f433885671245226
Camila Warmeling <camila.warmeling04@gmail.com>
2026-02-25 21:25:21 -0300

    Add files via upload

---
+++ Introdu-o-Programa-o-Furb-main/README.md
@@ -0,0 +1,2 @@
+# Int-Prog-Furb
+Destinado a projetos realizados durante a aula de Introdução à Programação na Furb no curso de BCC
---
+++ Introdu-o-Programa-o-Furb-main/aula1/aula1/src/TesteMenorValor.java
@@ -0,0 +1,11 @@
+public class TesteMenorValor {
+    public static void main(String[] args) {
+        int valor1 = 10;
+        int valor2 = 5;
+        int valor3 = 19;
+        if ((valor1 <= valor2) & (valor1 <= valor3)){
+            
+        }
+        
+    }
+}
