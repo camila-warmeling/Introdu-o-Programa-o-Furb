eafa1f6c4d47274c3d83c62ae5e605c764fa1b13
Camila Warmeling <camila.warmeling04@gmail.com>
2026-02-26 21:43:05 -0300

    Add files via upload

---
+++ Introdu-o-Programa-o-Furb-main/Introdu-o-Programa-o-Furb-main/aula1/aula1/src/TesteIdade.java
@@ -0,0 +1,9 @@
+public class TesteIdade {
+    public static void main(String[] args) {
+        int idade = 17;
+        if (idade >=18){
+            System.out.println("Adulto");
+        }
+        System.out.println("FIM");        
+    }
+}
